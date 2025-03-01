72e6ea2d8f165f473ce5c580df6eaccab477182e
AliciaJAL <93489683+AliciaJAL@users.noreply.github.com>
2025-02-28 21:49:05 -0800

    Update Title.js

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -24,8 +24,8 @@ class Title extends Phaser.Scene {
 
     create() {
         // add title text
-        this.add.bitmapText(centerX, centerY - 32, 'gem_font', 'THE ODYSSEY', 32).setOrigin(0.5)
-        this.add.bitmapText(centerX, centerY, 'gem_font', 'Press SPACE to start', 16).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY - 32, 'arcadeClassic_font', 'THE ODYSSEY', 32).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY, 'arcadeClassic_font', 'Press SPACE to start', 16).setOrigin(0.5)
 
         // create input
         cursors = this.input.keyboard.createCursorKeys()
